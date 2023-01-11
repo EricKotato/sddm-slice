589fef11c063bad84195c2050906c1cad60a00e7
Heimen Stoffels <vistausss@fastmail.com>
2023-01-11 22:45:56 +0100

    Added Dutch translation

---
+++ translations/nl.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl">
+<context>
+    <name>PagePower</name>
+    <message>
+        <source>Suspend</source>
+        <translation>Pauzestand</translation>
+    </message>
+    <message>
+        <source>Hibernate</source>
+        <translation>Slaapstand</translation>
+    </message>
+    <message>
+        <source>Hybrid Sleep</source>
+        <translation>Hybride slaapstand</translation>
+    </message>
+</context>
+<context>
+    <name>PageUsers</name>
+    <message>
+        <source>Back</source>
+        <translation>Terug</translation>
+    </message>
+</context>
+</TS>
